ecbe7a3c65a2b8854fd148269c6c782d04bc3b92
{arakiriki} <{szn0125@gmail.com}>
2021-05-11 22:58:19 +0900

    lesson2

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.views.generic import View
-from .models import Post
+from .models import Post,Category
 from .forms import PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -34,7 +34,12 @@ class CreatePostView(View,LoginRequiredMixin):
             post_data = Post()
             post_data.author = request.user
             post_data.title = form.cleaned_data['title']
+            category = form.cleaned_data['category']
+            category_data = Category.objects.get(name=category)
+            post_data.category = category_data
             post_data.content = form.cleaned_data['content']
+            if request.FILES:
+                post_data.image = request.FILES.get('image')
             post_data.save()
             return redirect('post_detail', post_data.id)            
 
@@ -50,7 +55,9 @@ class PostEditView(View,LoginRequiredMixin):
             request.POST or None,
             initial = {
                 'title':post_data.title,
-                'content':post_data.content
+                'category':post_data.category,
+                'content':post_data.content,
+                'image':post_data.image
             }
         )
 
@@ -64,7 +71,12 @@ class PostEditView(View,LoginRequiredMixin):
         if form.is_valid():
             post_data = Post.objects.get(id=self.kwargs['pk'])
             post_data.title = form.cleaned_data['title']
+            category = form.cleaned_data['category']
+            category_data = Category.objects.get(name=category)
+            post_data.category = category_data
             post_data.content = form.cleaned_data['content']
+            if request.FILES:
+                post_data.image = request.FILES.get('image')
             post_data.save()
             return redirect('post_detail', self.kwargs['pk'])            
 
@@ -84,3 +96,12 @@ class PostDeleteView(View,LoginRequiredMixin):
         post_data = Post.objects.get(id=self.kwargs['pk'])
         post_data.delete()
         return redirect('index')
+
+
+class CategoryView(View):
+    def get(self,request,*args,**kwargs):
+        category_data = Category.objects.get(name=self.kwargs['category'])
+        post_data = Post.objects.order_by('-id').filter(category=category_data)
+        return render(request,'app/index.html',{
+            'post_data':post_data
+        })
